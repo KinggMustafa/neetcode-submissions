ae6b3c34b48490dc9b6b14dc7a98c057ac6ef332
KinggMustafa <127482601+KinggMustafa@users.noreply.github.com>
2026-04-11 21:49:50 -0400

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        setnums = set(nums) #constant lookup using hashing
+        
+        sequence = 1
+        for i in range(len(nums)):
+            #how can we tell what the start of a sequence will be
+            cursequence = 1
+            if nums[i] -1 not in setnums: #we know a sequence has started
+                nextnum = nums[i] + 1
+                while (nextnum) in setnums:
+                    cursequence += 1
+                    nextnum += 1
+                #once we find a start we have to keep adding
+            sequence = max(sequence, cursequence) #keep track of our max sequence
+        return sequence
+
+        #the time compexity is o(n) time where n is the length of nums and o(n) space where n is the length of the set of nums 
+
+
