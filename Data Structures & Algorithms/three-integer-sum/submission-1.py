e6edc0c93d619f43d1d14b393a0e8457159915f6
KinggMustafa <127482601+KinggMustafa@users.noreply.github.com>
2026-04-23 02:02:45 -0400

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        #we use sorting to our advantage by using two pointers by comparing values and moving the pointers according to the overall sum
+        
+        output = []
+        for i in range(len(nums) -2):
+
+            #outer duplicate check:
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i + 1
+            right = len(nums) -1
+            while left < right:
+                overallsum = nums[i] + nums[left] + nums[right]
+                if overallsum == 0:
+                    output.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left + 1]: #we need 
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                elif overallsum < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return output
+
+
+        #time complexity: O(n^2) where n is the length of nums:
+        #nlogn for sorting times + o(n) (outer loop ) * o(n) (two pointer while loop)
+
+        #space O(1) since we use .sort() instead of storing a sorted nums
