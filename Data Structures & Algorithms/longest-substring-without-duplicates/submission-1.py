3c8d429dcb97661654cf5d8968333fc6e0f5187a
KinggMustafa <127482601+KinggMustafa@users.noreply.github.com>
2026-05-03 19:23:49 -0400

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        #brute force solution, check the longest substring starting at that given index
+        longest = 0
+        for i in range(len(s)):
+            substring = '' #at each index the substring starts
+            for j in range(i + 1, len(s)):
+                if s[j] not in substring:
+                    substring += s[j]
+                else:
+                    longest = max(len(substring), longest)
+                    continue
+        return longest
+        #o(n^2) time, o(n) space worst case if the entire array is not repeating
